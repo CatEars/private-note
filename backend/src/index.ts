db3a26a3b077e60fffbf4d99b7790ce3f1779179
Henke Adolfsson <catears13@gmail.com>
2019-12-17 21:38:51 +0100

    Final touches to hot reloading

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -120,7 +120,9 @@ const main = async () => {
         }
     })
 
-    app.listen(3000)
+    app.listen(3000, () => {
+        console.log('Listening on port 3000')
+    })
 }
 
 if (require.main === module) {
