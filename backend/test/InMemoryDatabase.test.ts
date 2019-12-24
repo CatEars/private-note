d92f4ef56493467595d772a33659f343185d497f
Henke Adolfsson <catears13@gmail.com>
2019-12-24 09:34:31 +0100

    Finish up tests for InMemoryDatabase

--- backend/test/InMemoryDatabase.test.ts
+++ backend/test/InMemoryDatabase.test.ts
@@ -1,5 +1,6 @@
 import 'jest'
 import * as _ from 'lodash'
+import uuid from 'uuid'
 
 import { InMemoryDatabase, Note, Log, NoteOptions } from '../src/database'
 
@@ -23,6 +24,12 @@ const normalOptions: NoteOptions = {
     checkAllowedReads: true,
     checkBurnDate: true,
     addAccess: true,
+}
+
+const exampleLog: Log = {
+    accessUrl: `http://localhost:3000/${uuid.v4()}`,
+    ip: '127.0.0.1',
+    timeOfAccess: Date.now(),
 }
 
 test('Can create db', () => {
@@ -59,6 +66,57 @@ test('Throws error on retrieve twice', async done => {
     done()
 })
 
+test('Throws error on old messages', async done => {
+    const db = new InMemoryDatabase()
+    const theOriginalNote = _.assign({}, exampleNote, {
+        burnDate: Date.now() - 1,
+    })
+    const id = await db.storeNote(theOriginalNote)
+    try {
+        await db.getNote(id, normalOptions)
+        fail()
+    } catch (err) {}
+    done()
+})
+
+test('Throws error on messages that do not exist', async done => {
+    const NUMBER_OF_NOTES = 10
+
+    const db = new InMemoryDatabase()
+
+    try {
+        // Should fail if it finds one and there are 0 notes in DB
+        await db.getNote(uuid.v4(), normalOptions)
+        fail()
+    } catch (err) {}
+
+    for (let idx = 0; idx < NUMBER_OF_NOTES; ++idx) {
+        const noteCopy = _.assign({}, exampleNote)
+        await db.storeNote(noteCopy)
+    }
+
+    try {
+        // Should fail if it finds one and there are 10 notes in DB
+        // note: uuids will never clash in practice
+        await db.getNote(uuid.v4(), normalOptions)
+        fail()
+    } catch (err) {}
+
+    done()
+})
+
+test('checkBurnDate:false can read old messages', async done => {
+    const db = new InMemoryDatabase()
+    const theOriginalNote = _.assign({}, exampleNote, {
+        burnDate: Date.now() - 1,
+    })
+    const oldRead = _.assign({}, normalOptions, { checkBurnDate: false })
+    const id = await db.storeNote(theOriginalNote)
+    const theNote = await db.getNote(id, oldRead)
+    expect(theNote).toEqual(theOriginalNote)
+    done()
+})
+
 test('CheckAllowedReads:false allows one to read multiple times', async done => {
     const multiRead = _.assign({}, normalOptions, { checkAllowedReads: false })
 
@@ -67,11 +125,72 @@ test('CheckAllowedReads:false allows one to read multiple times', async done => 
     const theNote = await db.getNote(id, normalOptions)
     expect(theNote).toEqual(exampleNote)
 
-    // Read four times
     await db.getNote(id, multiRead)
     await db.getNote(id, multiRead)
     await db.getNote(id, multiRead)
     await db.getNote(id, multiRead)
 
+    try {
+        await db.getNote(id, normalOptions)
+        fail()
+    } catch (err) {}
+
+    done()
+})
+
+test('Can store logs', async done => {
+    const db = new InMemoryDatabase()
+    const id = await db.storeLog(exampleLog)
+    const theLog = await db.getLog(id)
+    expect(theLog).toEqual(exampleLog)
+    done()
+})
+
+test('Throws error when log already exists', async done => {
+    const NUM_LOGS_STORED = 10
+    const db = new InMemoryDatabase()
+
+    try {
+        // Having 0 log entries should fail to retrieve the item
+        await db.getLog(uuid.v4())
+        fail()
+    } catch (err) {}
+
+    for (let idx = 0; idx < NUM_LOGS_STORED; ++idx) {
+        // Having 10 log entries should also fail to retrieve a random item
+        const logCopy = _.assign({}, exampleLog)
+        await db.storeLog(logCopy)
+    }
+
+    try {
+        await db.getLog(uuid.v4())
+        fail()
+    } catch (err) {}
+
+    done()
+})
+
+test('InMemoryDatabase can dump its database', async done => {
+    const db = new InMemoryDatabase()
+    expect(db.dumpDatabase()).toEqual({
+        notes: {},
+        accesses: {},
+        logs: {},
+    })
+
+    await db.storeNote(exampleNote)
+    expect(db.dumpDatabase()).not.toEqual({
+        notes: {},
+        accesses: {},
+        logs: {},
+    })
+    done()
+})
+
+test('Can store note with default options', async done => {
+    const db = new InMemoryDatabase()
+    const id = await db.storeNote(exampleNote)
+    const theNote = await db.getNote(id, {})
+    expect(theNote).toEqual(exampleNote)
     done()
 })
