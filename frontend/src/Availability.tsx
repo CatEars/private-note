764d9505637f9e1e8b7d6f049fe4bc8aee3da879
Henke Adolfsson <catears13@gmail.com>
2019-12-24 21:08:03 +0100

    Add availability checker to application

---
+++ frontend/src/Availability.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import _ from 'lodash'
+
+const necessaryElements = [
+    'crypto.getRandomValues',
+    'crypto.subtle.digest',
+    'crypto.subtle.generateKey',
+    'crypto.subtle.exportKey',
+    'crypto.subtle.importKey',
+    'crypto.subtle.encrypt',
+    'crypto.subtle.decrypt',
+]
+
+export const Availability = ({ children }) => {
+    const missing = necessaryElements.filter(x => !_.get(window, x))
+    if (missing.length) {
+        return (
+            <>
+                <p>
+                    Your browser does not seem to support all necessary
+                    functions
+                </p>
+                <p>Please use the latest version of Firefox or Chrome</p>
+                <p>The following browser features are missing:</p>
+                <ul>
+                    {missing.map(x => (
+                        <li>{x}</li>
+                    ))}
+                </ul>
+            </>
+        )
+    }
+    return <>{children}</>
+}
