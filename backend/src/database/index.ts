2d0ba6f2fb91ede3264845f2ad812f38d64a31fb
Henke Adolfsson <catears13@gmail.com>
2020-06-01 21:39:48 +0200

    Implement redis database

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -2,4 +2,6 @@ export { Database, Log, LogId, Note, NoteId, NoteOptions } from './types'
 
 export { InMemoryDatabase } from './InMemoryDatabase'
 
+export { RedisDatabase } from './RedisDatabase'
+
 export { unimplementedFunctionsOfDatabase } from './util'
